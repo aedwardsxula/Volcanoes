ef7de9fba3272194b7f2c13d12723d4c5636e3cc
Janya ward <janyaward33@gmail.com>
2025-11-25 15:43:14 -0600

    Add unittest for update_event_time to test_events.py

--- test_events.py
+++ test_events.py
@@ -38,7 +38,18 @@ class TestEventTimeMethods(unittest.TestCase):
         with self.assertRaises(ValueError):
             self.event.validate_event_time("2:00pm", "2:00pm")
 
-   
+    # update_event_time Tests
+    def test_update_event_time_success(self):
+        message = self.event.update_event_time("4:00pm", "5:30pm")
+
+        self.assertEqual(self.event.start_time, "4:00pm")
+        self.assertEqual(self.event.end_time, "5:30pm")
+        self.assertIsNotNone(self.event.updated_at)
+        self.assertIn("successfully updated", message.lower())
+
+    def test_update_event_time_failure(self):
+        with self.assertRaises(ValueError):
+            self.event.update_event_time("2:61pm", "3:00pm") 
 
     if __name__ == "__main__":
         unittest.main()
