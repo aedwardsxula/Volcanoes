cef7183961db199de56433b51b3aef2ee44c3bc6
Janya ward <janyaward33@gmail.com>
2025-11-30 15:57:46 -0600

    US #2 - Create ConflictChecker class and initialize attribute

--- conflict_checker.py
+++ conflict_checker.py
@@ -2,11 +2,4 @@ class ConflictChecker:
     def __init__(self, events):
         self.events = events
 
-    def check_conflict(self, location, new_start, new_end):
-        for event in self.events:
-            if event['location'] == location:
-                existing_start = event['start_time']
-                existing_end = event['end_time']
-                if (new_start < existing_end and new_end > existing_start):
-                    return True 
-        return False  
+    
