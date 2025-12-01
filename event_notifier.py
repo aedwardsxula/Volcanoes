1a7dce3f517374ff1d1c7b58f8231aff537182e0
Janya ward <janyaward33@gmail.com>
2025-12-01 01:48:45 -0600

    US #32 - Add subscribe method to EventNotifier class

--- event_notifier.py
+++ event_notifier.py
@@ -6,4 +6,12 @@ class EventNotifier:
         self.notification_log = []
         self.allowed_categories = categories or []
 
+    def subscribe(self, user_id, category=None):
+        if category is not None and category not in self.allowed_categories:
+            print(f"Category '{category}' is not allowed. Please choose one of the following: {self.allowed_categories}")
+            return
+
+        self.event_subscribers[user_id] = category
+        print(f"{user_id} subscribed to {'all events' if category is None else category}.")
+
     
